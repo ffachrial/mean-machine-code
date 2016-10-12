04a3f8a73ef031111615754f488cdc2152950ab4
Firman Fachrial <firman.fachrial@intikom.co.id>
2016-10-12 22:39:18 +0700

    Starting Node

---
+++ awesome-test/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('start', [], function() {
+    var options = {
+        script: 'server.js',
+        delayTime: 1,
+        watch: jsFiles
+    }
+
+    return nodemon(options)
+        .on('restart', function(err) {
+            console.log('Restarting...');
+        });
+});
