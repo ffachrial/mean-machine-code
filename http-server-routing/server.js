fc6ab833b59d376a17e9183d8aa44b7a02b901da
Firman Fachrial <firman.fachrial@intikom.co.id>
2016-10-13 12:08:32 +0700

    Login Route (app.route())

--- http-server-routing/server.js
+++ http-server-routing/server.js
@@ -62,3 +62,15 @@ adminRouter.get('/users/:name', function(req, res) {
 
 // apply the routes to our application
 app.use('/admin', adminRouter);
+
+app.route('/login')
+    // show the form (GET http://localhost:1337/login)
+    .get(function(req, res) {
+        res.send('this is the login form');
+    })
+
+    // process the form (POST http://localhost:1337/login)
+    .post(function(req, res) {
+        console.log('processing');
+        res.send('processing the login form!');
+    });
