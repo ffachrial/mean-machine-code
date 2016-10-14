1fd4f6d94e82c1433f571084b017f7f702d213d4
Firman Fachrial <firman.fachrial@intikom.co.id>
2016-10-14 17:11:14 +0700

    Route Middleware

--- node-api/server.js
+++ node-api/server.js
@@ -41,6 +41,18 @@ app.get('/', function(req, res) {
 // get an instance of the express router
 var apiRouter = express.Router();
 
+// middleware to use for all requests
+apiRouter.use(function(req, res, next) {
+    // do logging
+    console.log('Somebody just came to our app!');
+
+    // we'll add more to the middleware in Chapter 10
+    // this is were we will authenticate our users
+
+    // make sure we go to the next routes and don't stop here
+    next(); 
+});
+
 // test route to make sure everything is working
 // accessed at GET http://localhost:8080/api
 apiRouter.get('/', function(req, res) {
