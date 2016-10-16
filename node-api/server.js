71ed6b4d3d093ede4428f525549a7eeb90ca5634
Firman Fachrial <firman.fachrial@intikom.co.id>
2016-10-16 23:39:08 +0700

    Updating a User's Info (PUT /api/users/:user_id)

--- node-api/server.js
+++ node-api/server.js
@@ -111,6 +111,30 @@ apiRouter.route('/users/:user_id')
             // return the user
             res.json(user);
         });
+    })
+    
+    // update the user with this id
+    // (accessed at PUT http://localhost:8080/api/users/:user_id)
+    .put(function(req, res) {
+
+        // use our user model to find the user we want
+        User.findById(req.params.user_id, function(err, user) {
+
+            if (err) return res.send(err);
+
+            // update the user info only if its new
+            if (req.body.name) user.name = req.body.name;
+            if (req.body.username) user.username = req.body.username;
+            if (req.body.password) user.password = req.body.password;
+
+            // save the user
+            user.save(function (err) {
+                if (err) return res.send(err);
+
+                // return a message
+                res.json({ message: 'User updated!' });
+            });
+        });
     });
 
 // REGISTER OUR ROUTES -----------------------------------
