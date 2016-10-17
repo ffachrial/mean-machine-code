8f5616c8c119ef1e9f32584b4b2e132917add36e
Firman Fachrial <firman.fachrial@intikom.co.id>
2016-10-17 14:51:32 +0700

    Authenticating A User and Giving a Token

--- node-authentication/server.js
+++ node-authentication/server.js
@@ -45,6 +45,55 @@ app.get('/', function(req, res) {
 // get an instance of the express router
 var apiRouter = express.Router();
 
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRouter.post('/authenticate', function(req, res) {
+
+    // find the user
+    // select the name username and password explicitly
+    User.findOne({
+        username: req.body.username
+    }).select('name username password').exec(function(err, user) {
+
+        if (err) throw err;
+
+        // no user with that username was found
+        if (!user) {
+            res.json({
+                success: false,
+                message: 'Authentication failed. User not found.'
+            });
+        } else if (user) {
+
+            // check if password matches
+            var validPassword = user.comparePassword(req.body.password);
+            if (!validPassword) {
+                res.json({
+                    success: false,
+                    message: 'Authentication failed. Wrong password.'
+                });
+            } else {
+
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign({
+                    name: user.name,
+                    username: user.username
+                }, superSecret, {
+                    //expiresInMinutes: 1440  // expires in 24 hours (UPDATE : 2016-10-17. for my case, it have to call expiresIn rather than expiresInMinutes)
+                    expiresIn: 60*24    // expires in 24 hours
+                });
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+        }
+    });
+});
+
 // middleware to use for all requests
 apiRouter.use(function(req, res, next) {
     // do logging
