48a2263073ffde5a3c88ada71d3614e6775221ab
Firman Fachrial <firman.fachrial@intikom.co.id>
2016-10-19 12:39:22 +0700

    Main Application

---
+++ user-crm/public/app/app.js
@@ -0,0 +1,8 @@
+angular.module('userApp', [
+    'ngAnimate',            // add animation
+    'app.routes',           // routing for application
+    'authService',          
+    'mainCtrl',
+    'userCtrl',
+    'userService'
+]);
