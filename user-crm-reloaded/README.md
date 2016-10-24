ea60aa90959f4f515194280215212109aa5a51d5
Firman Fachrial <firman.fachrial@intikom.co.id>
2016-10-24 10:11:16 +0700

    Create BASIC SERVER

---
+++ user-crm-reloaded/README.md
@@ -0,0 +1,13 @@
+# H2 Introduction
+First weeks of the October 2016 I followed the course of the ME_N Stack. (not included the 'A' for Angular)
+
+# H3 Create Basic Server
+1. Create standard **package.json** file by *npm init*
+2. Install Express by *npm install --save express* 
+3. Create **config.js**
+  1. Add port for using by server.js
+4. Create blank **index.html** on *public/app/views/index.html*
+5. Create **server.js**
+  1. Call basic packages (included express and config)
+  2. Create basic ROUTE to FRONTEND
+  3. Start the SERVER by listening to port from config
---
+++ user-crm-reloaded/server.js
@@ -0,0 +1,20 @@
+// BASE SETUP
+// ======================================
+
+// CALL THE PACKAGES --------------------
+var express = require('express');   // call express
+var app     = express();            // define app using express
+var path    = require('path');      // used for delimiter '/'
+var config  = require('./config');  // call config for parameters
+
+// MAIN CACTHALL ROUTE ------------------
+// SEND USERS TO FRONTEND ---------------
+// has to be registered after API ROUTES
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+});
+
+// START THE SERVER
+// ======================================
+app.listen(config.port);
+console.log('Listening on port ' + config.port);
