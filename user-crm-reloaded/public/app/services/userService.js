87505b204ed67943a8987597dd403ca9e714b5bb
Firman Fachrial <firman.fachrial@intikom.co.id>
2016-10-27 10:46:32 +0700

    Add Services for PUT User data

--- user-crm-reloaded/public/app/services/userService.js
+++ user-crm-reloaded/public/app/services/userService.js
@@ -7,6 +7,11 @@ angular.module('userService', [])
         // create a new object
         var userFactory = {};
 
+        // get a single user
+        userFactory.get = function(id) {
+            return $http.get('/api/users/' + id);
+        };
+
         // get all users
         userFactory.all = function() {
             return $http.get('/api/users/');
@@ -16,7 +21,12 @@ angular.module('userService', [])
         userFactory.create = function(userData) {
             return $http.post('/api/users/', userData);
         };
-        
+
+        // update a user
+        userFactory.update = function(id, userData) {
+            return $http.put('/api/users/' + id, userData);
+        };
+
         // return our entire userFactory object
         return userFactory;
         
