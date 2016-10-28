73984996aa236e9199a2ced349daae170f0de732
Firman Fachrial <firman.fachrial@intikom.co.id>
2016-10-28 14:01:36 +0700

    Add API for user info on Navbar

--- user-crm-reloaded/app/routes/api.js
+++ user-crm-reloaded/app/routes/api.js
@@ -64,6 +64,45 @@ module.exports = function(app, express) {
         });
     });
 
+	// route middleware to verify a token
+	apiRouter.use(function(req, res, next) {
+		// do logging
+		console.log('Somebody just came to our app!');
+
+	  // check header or url parameters or post parameters for token
+	  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+	  // decode token
+	  if (token) {
+
+	    // verifies secret and checks exp
+	    jwt.verify(token, superSecret, function(err, decoded) {      
+
+	      if (err) {
+	        res.status(403).send({ 
+	        	success: false, 
+	        	message: 'Failed to authenticate token.' 
+	    	});  	   
+	      } else { 
+	        // if everything is good, save to request for use in other routes
+	        req.decoded = decoded;
+	            
+	        next(); // make sure we go to the next routes and don't stop here
+	      }
+	    });
+
+	  } else {
+
+	    // if there is no token
+	    // return an HTTP response of 403 (access forbidden) and an error message
+   	 	res.status(403).send({ 
+   	 		success: false, 
+   	 		message: 'No token provided.' 
+   	 	});
+	    
+	  }
+    });
+
     // test route to make sure everything is working
     // accessed at GET http://localhost:port_number/api
     apiRouter.get('/', function(req, res) {
@@ -154,6 +193,11 @@ module.exports = function(app, express) {
                     res.json({ message : 'Successfully deleted' });
             });
         });
+
+	// api endpoint to get user information
+	apiRouter.get('/me', function(req, res) {
+		res.send(req.decoded);
+	});
 
     return apiRouter;
 };
